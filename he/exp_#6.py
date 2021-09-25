e88a2a03468c30e09503b49ab301fd78f25ab86e
Nikoloz Tvildiani <nikoloz.tvildiani@dtmu.edu.ge>
2021-09-26 01:44:53 +0400

    majr changes in data analysis

--- he/exp_#6.py
+++ he/exp_#6.py
@@ -1,17 +1,18 @@
-import datetime 
-import os
-import shutil
-import pickle
-import Internet_protocols
+import logging.handlers
 import logging
 import time
-import pathlib
-import logging.handlers
-from iqoptionapi.stable_api import IQ_Option
-from tqdm import tqdm
 import sys
+import os
+import pickle
+import pathlib
+import csv
+import tqdm
+import datetime
+import requests
+import itertools
+import statistics
+import json
 '''----------------------------------------------------------------------------------------------'''
-
 if os.name == 'posix':
     path = pathlib.PurePosixPath(os.path.abspath(__file__)).parent 
     if 'Forex_experiments' in path.parts:
@@ -28,7 +29,7 @@ else:
         path = path / 'Forex_experiments'  / str(datetime.date.today())
     # After this, path is equals to current date folder
     file_path = pathlib.PureWindowsPath(os.path.abspath(__file__))
-    
+
 try:
     os.mkdir(str(path))
 except OSError as ose:
@@ -40,8 +41,6 @@ except FileNotFoundError:
     experiment_number = 1
 pickle.dump(experiment_number, open(str(path.parent / 'experiment_number.pkl'), 'wb+'))
 
-shutil.copy(file_path, path / (file_path.stem + str(experiment_number) + file_path.suffix) )
-
 '''----------------------------------------------------------------------------------------------'''
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -52,73 +51,60 @@ stream_handler.setLevel(logging.DEBUG)
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
 """FileHandler"""
-rotatingfile_handler = logging.handlers.RotatingFileHandler(path.parent/'main.log', backupCount=5, maxBytes=1073741824)
+rotatingfile_handler = logging.handlers.RotatingFileHandler(path/'main.log', backupCount=5, maxBytes=1073741824)
 rotatingfile_handler.setLevel(logging.DEBUG)
 rotatingfile_handler.setFormatter(formatter)
 logger.addHandler(rotatingfile_handler)
-
-logger.info('Main Entry ' + str(experiment_number))
-'''----------------------------------------------------------------------------------------------'''
-
-sleep_time = 60*3
+#----------------------------------------------------------------------------#
+logger.info(file_path.stem + '  ' + str(experiment_number))
 
 
-if __name__ == '__main__':
+while True:
     try:
-        Internet_protocols.checkInternetRequests()
+        markets = []
+        with open(file_path.parent / 'physical_currency_list.csv', 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                markets.append(row['currency code'])
 
-        connector =IQ_Option("ww.bingonemo@gmail.com","JF*#3C5va&_NDqy")
-        connector.connect()
+        comb = itertools.combinations(markets, 2)
 
-        connector.update_ACTIVES_OPCODE()
-        ALL_Asset = connector.get_all_open_time()
-       
+        iqcomb = [("NZD", "SEK"),("NZD", "USD"),("CAD", "TRY"),("EUR", "CHF"),("USD", "THB"),("EUR", "TRY"),("GBP", "AUD"),("AUD", "USD"),("EUR", "MXN"),("USD", "CHF"),("NOK", "DKK"),("EUR", "NOK"),("SEK", "JPY"),("CAD", "NOK"),("NOK", "SEK"),("GBP", "HUF"),("GBP", "SGD"),("AUD", "NZD"),("GBP", "JPY"),("CHF", "SEK"),("AUD", "NOK"),("GBP", "NOK"),("AUD", "DKK"),("EUR", "AUD"),("AUD", "CHF"),("GBP", "CHF"),("AUD", "CAD"),("CHF", "DKK"),("AUD", "TRY"),("NZD", "CHF"),("USD", "SEK"),("GBP", "NZD"),("EUR", "DKK"),("NZD", "DKK"),("CAD", "SGD"),("EUR", "GBP"),("EUR", "CAD"),("USD", "CZK"),("AUD", "MXN"),("EUR", "NZD"),("GBP", "PLN"),("NZD", "NOK"),("AUD", "SGD"),("GBP", "SEK"),("NZD", "CAD"),("NZD", "MXN"),("NZD", "TRY"),("CHF", "SGD"),("USD", "MXN"),("EUR", "HUF"),("GBP", "CAD"),("USD", "TRY"),("USD", "JPY"),("EUR", "USD"),("AUD", "SEK"),("CHF", "NOK"),("USD", "PLN"),("USD", "HUF"),("CHF", "JPY"),("GBP", "ILS"),("NZD", "JPY"),("CHF", "TRY"),("CAD", "JPY"),("USD", "RUB"),("SGD", "JPY"),("GBP", "USD"),("CAD", "PLN"),("DKK", "SGD"),("NZD", "SGD"),("AUD", "JPY"),("NOK", "JPY"),("PLN", "SEK"),("USD", "SGD"),("GBP", "MXN"),("USD", "CAD"),("SEK", "DKK"),("DKK", "PLN"),("CAD", "MXN"),("GBP", "TRY"),("EUR", "SGD"),("NZD", "ZAR"),("EUR", "CZK"),("EUR", "JPY"),("CAD", "CHF"),("USD", "INR"),("USD", "BRL"),("USD", "NOK"),("USD", "DKK")]
 
-        date = datetime.date(year= 2021, month=8, day=18)  # day must be + 1,beacuse this is end date
-        end_from_time = time.mktime(date.timetuple())
-        
-        
-        # Volatility with standart deviation
-      
-        past_time_vol_assets = []
-        for f in tqdm(ALL_Asset['forex'].keys()):
-            data = connector.get_candles(f, 86400, 200, end_from_time)
+        result = []
+        for f in tqdm.tqdm(iqcomb):
+            trail = 0
+            while True:
+                try:
+                    url = 'https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=' + f[trail%2] + '&to_symbol=' + f[(trail + 1)%2] + '&outputsize=full&apikey=PNA2KC2F5LETFRF1'
+                    r = requests.get(url)
+                    data = r.json()
+                    if 'Error Message' in data:
+                        tqdm.tqdm.write(str((f[trail%2], f[(trail + 1)%2])) + str(data))
+                        trail += 1
+                    elif 'Note' in data:
+                        # tqdm.tqdm.write(str((f[trail%2], f[(trail + 1)%2])) + str(data))
+                        for sl in tqdm.tqdm(range(60), leave=False):
+                                time.sleep(1)
+                    else:
+                        close_data = [float(x['4. close']) for x in list(data.values())[1].values()]
+                        tqdm.tqdm.write(str(((f[trail%2], f[(trail + 1)%2]), statistics.mean(close_data))))
+                        result.append(((f[trail%2], f[(trail + 1)%2]), statistics.mean(close_data) ))
+                        break
+                except:
+                    trail += 1
+
+        with open(file_path.parent / 'json_examples/market_mean.json', 'w') as jsonfile:
+            json.dump(result, jsonfile)
 
 
-            percList = []
-            for candle in data:
-                percent = (candle.get('max') - candle.get('min'))/ candle.get('min')
-                percList.append(percent)
-
-
-            volatility = sum(percList)/len(percList)
-
-
-            # storting 
-            count = 0
-            for ps in past_time_vol_assets:
-                if ps[1] < volatility:
-                    past_time_vol_assets = past_time_vol_assets[:count] + [(f, volatility)] + past_time_vol_assets[count:]
-                    break
-                count += 1
-            else:
-                past_time_vol_assets.append((f, volatility))
-        past_time_assets = [x[0] for x in past_time_vol_assets ]
-        data_assets =  [x[1] for x in past_time_vol_assets ]
-        print (past_time_assets)
-        print( past_time_vol_assets)
-        print(sum(data_assets)/len(data_assets))
-
+        break
 
     except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
         logger.exception(str(e))
         logger.exception([exc_type, fname, exc_tb.tb_lineno])
-        Internet_protocols.email(str(e), subj='Error')
-        time.sleep(sleep_time)
-        logger.info('Restart')
-        if os.name == 'posix':
-            os.system('sudo reboot')
-        else:
-            os.system('shutdown /r')
+        time.sleep(60*3)
+
+
---
+++ he/sandbox.py
@@ -0,0 +1,7 @@
+from scipy import stats
+
+print(stats.poisson.pmf(3 , 1.19))
+print(stats.gamma.pdf(1.19 , 4))
+
+
+
